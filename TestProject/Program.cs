c82a3f9d863373b6734dcc997f7b510708461fbc
Anastasiia.Posulikhi <anastasiia.posulikhina@jetbrains.com>
2022-06-23 13:45:47 +0300

    commit 2

---
+++ TestProject/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Moq;
+
+var mock = new Mock<IRepository>();
+mock.Setup(repo=>repo.GetAll()).Returns(new List<User>
+{
+    new User { Id=1, Name="Tom", Age=35},
+    new User { Id=2, Name="Alice", Age=29},
+    new User { Id=3, Name="Sam", Age=32},
+    new User { Id=4, Name="Kate", Age=30}
+});
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public interface IRepository
+{
+    IEnumerable<User> GetAll();
+    User Get(int id);
+    void Create(User user);
+    
+    
+}
